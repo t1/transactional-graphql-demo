afc649a7d734596476f6a198e584535e6d3224ed
Rüdiger zu Dohna <snackbox@sinntr.eu>
2020-09-10 07:40:35 +0200

    add mutations and wrapper-mutation

--- src/main/java/com/example/transactional/graphql/GraphQlBoundary.java
+++ src/main/java/com/example/transactional/graphql/GraphQlBoundary.java
@@ -1,6 +1,7 @@
 package com.example.transactional.graphql;
 
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.Query;
 import org.eclipse.microprofile.graphql.Source;
 
@@ -15,5 +16,22 @@ public class GraphQlBoundary {
 
     public String bar(@Source Result result) {
         return service.foo();
+    }
+
+
+    @Mutation public String foo(String input) { return input + service.foo(); }
+
+    @Mutation public String bar(String input) { return input + service.foo(); }
+
+
+    @Mutation public MutationWrapper combined(MutationWrapper wrapper) {
+        if (wrapper.foo != null) wrapper.foo = "wrapped:" + foo(wrapper.foo);
+        if (wrapper.bar != null) wrapper.bar = "wrapped:" + bar(wrapper.bar);
+        return wrapper;
+    }
+
+    public static class MutationWrapper {
+        public String foo;
+        public String bar;
     }
 }
