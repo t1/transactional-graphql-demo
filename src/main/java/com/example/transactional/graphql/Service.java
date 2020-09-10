769e95e780cb09cd988bfa37b7e5cdf17f44ac8d
Rüdiger zu Dohna <snackbox@sinntr.eu>
2020-09-10 05:02:47 +0200

    removed non-functional TransactionAttribute annotation

--- src/main/java/com/example/transactional/graphql/Service.java
+++ src/main/java/com/example/transactional/graphql/Service.java
@@ -1,16 +1,12 @@
 package com.example.transactional.graphql;
 
-import javax.ejb.TransactionAttribute;
 import javax.inject.Inject;
-
-import static javax.ejb.TransactionAttributeType.MANDATORY;
 
 public class Service {
 
     @Inject TransactionScopedBean transactionScopedBean;
     @Inject RequestScopedBean requestScopedBean;
 
-    @TransactionAttribute(MANDATORY)
     public String foo() {
         return "[" +
             "transaction:" + transactionScopedBean.getId() + " " +
